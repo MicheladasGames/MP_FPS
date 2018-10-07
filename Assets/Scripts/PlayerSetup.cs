471e460d018726b4fc730064ac9e4fbe486ac3bc
Andres Hernandez <andresjhe@gmail.com>
2018-10-07 15:17:58 +0100

    Shooting and hitting box fixed

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -5,17 +5,16 @@ public class PlayerSetup : NetworkBehaviour
 {
     [SerializeField]
     Behaviour[] disabledComponents;
-
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
     Camera sceneCamera;
 
     private void Start()
     {
         if (!isLocalPlayer)
         {
-            for (int i = 0; i < disabledComponents.Length; i++)
-            {
-                disabledComponents[i].enabled = false;
-            }
+            DisableComponents();
+            AssignRemoteLayer();
         } else
         {
             sceneCamera = Camera.main;
@@ -24,6 +23,28 @@ public class PlayerSetup : NetworkBehaviour
                 sceneCamera.gameObject.SetActive(false);
             }
             
+        }
+
+        
+        RegisterPlayer();
+    }
+
+    void RegisterPlayer()
+    {
+        string _Id = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _Id;
+    }
+
+    private void AssignRemoteLayer ()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    private void DisableComponents()
+    {
+        for (int i = 0; i < disabledComponents.Length; i++)
+        {
+            disabledComponents[i].enabled = false;
         }
     }
     private void OnDisable()
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Serializeble makes it possible to be used in other classes in Unity
+[System.Serializable]
+public class PlayerWeapon {
+
+    public string name = "Glock";
+    public float damage = 10f;
+    public float range = 100f;
+
+}
